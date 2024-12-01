d61c6e56ec63bde58adbf33602a8366eb69fbca4
hajimins <minura.hajisoy@gmail.com>
2024-12-01 15:38:37 -0500

    adding Persistence project

---
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,17 @@
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+     public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+          IConfiguration configuration)
+     { 
+          services.AddDbContext<HrDatabaseContext>(options =>
+               options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString")));
+          return services;
+     }
+}
