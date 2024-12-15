4c04a139d22e47f18b20ff7af14f749cbde60115
hajimins <minura.hajisoy@gmail.com>
2024-12-14 21:33:40 -0500

    adding emailservices

--- HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
+++ HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Models.Email;
+using HR.LeaveManagement.Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HR.LeaveManagement.Infrastructure;
@@ -8,6 +11,8 @@ public static class InfrastructureServicesRegistration
     public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection
         services, IConfiguration configuration)
     {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailSender, EmailSender>();
         return services;
     }
-}
+}  
